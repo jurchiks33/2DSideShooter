3ca3e9912e79df5de19502ca31c7c92951ab5524
jurchiks33 <jurisslisans@gmail.com>
2023-12-18 17:25:42 +0100

    first commit

---
+++ game_framework.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+pygame.init()
+
+#Game constants.
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+BG_COLOR = (50, 50, 50)
+PLAYER_COLOR = (255, 0, 0)
+PLAYER_SIZE = 50
+GRAVITY = 0.5
+JUMP_STRENGTH = 10
